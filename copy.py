263a975c3f37bfbc734485dce281f8bac281eae6
Adithyan R <80190745+Adithyan-R@users.noreply.github.com>
2022-05-24 12:39:20 +0530

    Add files via upload

--- copy.py
+++ copy.py
@@ -1,39 +1,104 @@
 import csv
 
-def csv_dict_writer(path, fieldnames, data):
-    with open(path, "wb") as out_file:
-        writer = csv.DictWriter(out_file, delimiter=',', fieldnames=fieldnames)
-        writer.writeheader()
-        for row in data:
-            writer.writerow(row)
+try:
+	def write_csv_from_list_dict(filename, table, fieldnames, separator, quote):
+		
+		"""
+		Inputs:
+		filename   - name of CSV file
+		table      - list of dictionaries containing the table to write
+		fieldnames - list of strings corresponding to the field names in order
+		separator  - character that separates fields
+		quote      - character used to optionally quote fields
+		Output:
+		Writes the table to a CSV file with the name filename, using the
+		given fieldnames.  The CSV file should use the given separator and
+		quote characters.  All non-numeric fields will be quoted.
+		"""
+		with open(filename, 'w', newline='') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=fieldnames,
+											delimiter=separator,
+											quotechar=quote,
+											quoting=csv.QUOTE_NONNUMERIC)        
+			writer.writeheader()
+			for value in table:
+				writer.writerow({fieldnames[0]: value[fieldnames[0]],
+								fieldnames[1]: value[fieldnames[1]],
+								})            
 
-empty_dict = {}  
-empty_list = []
-fieldnames = ['Symbol', 'Ukey', 'Qty', 'MarketVolume', 'MarketShare']
-f_ob = open("20140923_MarketVolume.csv")
-f_obj = open("20140923_MarketShare.csv")
-reade = csv.DictReader(f_ob, delimiter=',')
-reader = csv.DictReader(f_obj, delimiter=',')
-path = "result.csv"
-errors = 0
 
-for line in reade:
- 		a = line["UKey"]
- 		b = line["Volume"]
- 		empty_dict[a] = b
+except FileNotFoundError:
+	msg='FileNotFoundError'
 
-for line in reader:
- 	empty_list.append(line)
+finally:
+	print(write_csv_from_list_dict('table1.csv', [{'col1': 'data7', 'col2': 'data8'}, \
+													{'col1': 'data9', 'col2': 'data10'}, \
+												], \
+													['col1', 'col2'], ',', '"'))
 
-for i in empty_list:
-	h = i["Ukey"]
-	
-	try:
-		i["MarketVolume"] = empty_dict[h] 
-	
-	except:
-		errors += 1
 
-print errors, "errors"
 
-csv_dict_writer(path, fieldnames, empty_list)
+try:
+	def read_csv_fieldnames(filename, separator, quote):
+		"""
+		Inputs:
+		filename  - name of CSV file
+		separator - character that separates fields
+		quote     - character used to optionally quote fields
+		Ouput:
+		A list of strings corresponding to the field names in
+		the given CSV file.
+		"""
+    
+		with open(filename, newline='') as csvfile:
+				
+			reader = csv.DictReader(csvfile,
+									delimiter=separator,
+									quotechar=quote)
+			
+			return reader.fieldnames
+
+except FileNotFoundError:
+	msg= 'File does not exist'
+	print(msg)
+
+finally:
+	print(read_csv_fieldnames("table1.csv", ",", ";"))
+
+
+
+try:
+	def read_csv_as_list_dict(filename, separator, quote):
+		"""
+		Inputs:
+		filename  - name of CSV file
+		separator - character that separates fields
+		quote     - character used to optionally quote fields
+		Output:
+		Returns a list of dictionaries where each item in the list
+		corresponds to a row in the CSV file.  The dictionaries in the
+		list map the field names to the field values for that row.
+		"""   
+    
+		list_of_values = []
+		
+		with open(filename, newline='') as csvfile:
+			reader = csv.DictReader(csvfile,
+									delimiter=separator,
+									quotechar=quote)
+			for row in reader:
+				values = {}
+				for name in row:
+					values[name] = row[name]
+				list_of_values.append(values)
+				
+		return list_of_values
+
+
+except FileNotFoundError:
+	msg='File does not exist'
+	print(msg)
+
+
+finally:
+	print(read_csv_as_list_dict("table1.csv", ",", ";"))
